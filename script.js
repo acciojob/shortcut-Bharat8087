9262cf0f06941ad681e6b348172ad09c1d5c6414
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-03-29 14:51:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function shortcut(s1, s2) {
-  // your code here
+  if (s1.length === 0 || s2.length === 0) {
+ return''; } else {
+    return s1[0].toUpperCase() + s2[0].toUpperCase();
+  }
 }
 
-// Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
